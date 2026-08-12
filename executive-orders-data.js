fcc93278d9045064836252d9cdfdd00415216d35
onzok <0nz0k.rblx@gmail.com>
2026-08-12 06:17:27 +0300

    Add files via upload

--- executive-orders-data.js
+++ executive-orders-data.js
@@ -6,6 +6,7 @@ const EXECUTIVE_ORDERS_DATA = [
     governor: 'ONLYTWENTYCHARACTERS',
     administration: 'ONLYTWENTYCHARACTERS Administration',
     color: '#e7ddd4',
+    status: 'ACTIVE',
     provisions: [
       { sec: '§ 1', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
       { sec: '§ 2', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
@@ -20,6 +21,7 @@ const EXECUTIVE_ORDERS_DATA = [
     governor: 'ONLYTWENTYCHARACTERS',
     administration: 'onzok Administration',
     color: '#e7ddd4',
+    status: 'RETRACTED',
     provisions: [
       { sec: '§ 1', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
       { sec: '§ 2', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
